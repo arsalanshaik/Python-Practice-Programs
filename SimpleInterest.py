a43f2fd807c690debaf3937265dfc01b45c2ed36
Arsalan Shaikh <59007087+arsalanshaik@users.noreply.github.com>
2019-12-18 13:48:56 +0530

    Add files via upload

---
+++ SimpleInterest.py
@@ -0,0 +1,7 @@
+p=float(input("Enter the Principle Amount:"))
+n=float(input("Enter the duration in years"))
+r=float(input("Enter the Rate of Interest Per Annum:"))
+
+simple_interest=p*n*r/100
+
+print("simple_interest is {}".format(simple_interest))
